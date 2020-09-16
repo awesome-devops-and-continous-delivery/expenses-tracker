7453d7adce4cb743e7eb70390cea5b7af18cde48
Flavio Primo <fla.primo.engineer@outlook.com>
2020-09-16 11:13:39 +0200

    added add expenses endpoint

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
   .use(express.static('web'))
 
 const lists = []
+const lists_dict = {}
 
 app.get('/lists', function (req, res) {
   res.json(lists)
@@ -20,8 +21,27 @@ app.post('/lists', function (req, res) {
   const list = req.body
   list.id = lists.length
   lists.push(list)
+  lists_dict[list.id] = []
   res.setHeader('Location', '/lists/' + list.id)
   res.json(list)
+})
+
+app.post('/lists/:id/expenses', function (req, res) {
+  var cat_list = lists_dict[req.params.id]
+
+  if (cat_list) {
+    const expense = req.body
+    expense.id = cat_list.length
+
+    cat_list.push(expense)
+    console.log('added %s expense', expense.id)
+
+    //res.setHeader('Location', '/lists/' + list.id)
+    res.status(201)
+    res.json(cat_list)
+  } else {
+    console.log('list with id %s doesn\'t exist you goat', req.params.id)
+  }
 })
 
 app.listen(process.env.npm_package_config_port, function () {
